a7b4dda04f8609e9422ee6e31576ef7f961ccfe6
youngjunlee1120 <youngjunlee1120@gmail.com>
2025-08-12 22:50:29 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import UseEffect from "./UseEffect";
-import Button from "./Button";
-import CleanUp from "./CleanUp";
-import ToDoList from "./ToDoList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
